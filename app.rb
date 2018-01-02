736eb07ef0199246d7a89a7d22fd6672f8061bbd
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2018-01-02 22:01:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
+  #This is a route
+  get '/' do
+  #Here starts the action of the route
+    "Hello, World!"
+  end
+
 
 end
